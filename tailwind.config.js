ad42d2f7efa75e36807be32c9ccf63af8a9a69f8
Przemek Gulik <gulikdev@gmail.com>
2024-06-19 16:15:38 +0200

    feat: daisy ui init with theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,34 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#1D3557",
+
+          "secondary": "#457B9D",
+
+          "accent": "#E63946",
+
+          "neutral": "#FFFFFF",
+
+          "base-100": "#ffffff",
+
+          "info": "#A8DADC",
+
+          "success": "#00ff00",
+
+          "warning": "#00ff00",
+
+          "error": "#E63946"
+        },
+      },
+    ],
+  },
+  plugins: [
+    require('daisyui')
+  ]
 }
 
