cd10e58cb5eb939f40d329bc59b90427cd87f82f
Przemek Gulik <gulikdev@gmail.com>
2024-06-22 20:58:34 +0200

    feat: datepicker, done quick filters ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {MenuComponent} from "./components/menu/menu.component";
 import {MobileMenuComponent} from "./components/mobile-menu/mobile-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, MenuComponent, MobileMenuComponent],
+  imports: [RouterOutlet, MenuComponent, MobileMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'inzynier-front';
 }
--- src/app/components/shared/quick-filters/quick-filters.component.ts
+++ src/app/components/shared/quick-filters/quick-filters.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {MatDatepickerModule,} from "@angular/material/datepicker";
+import {FormControl, FormGroup} from "@angular/forms";
+import {DateAdapter, MAT_DATE_LOCALE} from "@angular/material/core";
+import {MatInput, MatSuffix} from "@angular/material/input";
 
 @Component({
   selector: 'app-quick-filters',
   standalone: true,
-    imports: [
-        NgOptimizedImage
-    ],
+  imports: [
+    NgOptimizedImage,
+    MatDatepickerModule,
+    MatInput,
+    MatSuffix
+  ],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'en-GB'}
+  ],
   templateUrl: './quick-filters.component.html',
   styleUrl: './quick-filters.component.scss'
 })
-export class QuickFiltersComponent {
+export class QuickFiltersComponent implements OnInit {
 
+  readonly range = new FormGroup({
+    start: new FormControl<Date | null>({value: null, disabled: true}),
+    end: new FormControl<Date | null>({value: null, disabled: true}),
+  });
+
+  constructor(
+    private readonly dateAdapter: DateAdapter<Date>
+  ) {
+  }
+
+  ngOnInit() {
+    this.dateAdapter.setLocale('en-GB');
+  }
 }
