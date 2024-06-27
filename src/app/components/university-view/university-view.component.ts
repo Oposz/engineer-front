94a051256871366eeec88438dc2a81acc1146264
Przemek Gulik <gulikdev@gmail.com>
2024-06-27 18:52:22 +0200

    feat: university view

--- src/app/components/university-view/university-view.component.ts
+++ src/app/components/university-view/university-view.component.ts
@@ -1,12 +1,30 @@
 import {Component} from '@angular/core';
+import {NgScrollbar} from "ngx-scrollbar";
+import {QuickFiltersComponent} from "../shared/quick-filters/quick-filters.component";
+import {UniversityCardComponent} from "../universities/university-card/university-card.component";
+import {ViewHeaderComponent} from "../shared/view-header/view-header.component";
+import {ProjectCardComponent} from "../open-projects/project-card/project-card.component";
+import {Project} from "../../shared/constants/project";
+import * as mockData from "../open-projects/mock-projects.json";
+import {RouterLink} from "@angular/router";
 
 @Component({
-  selector: 'app-univeristy-view',
+  selector: 'app-university-view',
   standalone: true,
-  imports: [],
+  imports: [
+    NgScrollbar,
+    QuickFiltersComponent,
+    UniversityCardComponent,
+    ViewHeaderComponent,
+    ProjectCardComponent,
+    RouterLink
+  ],
   templateUrl: './university-view.component.html',
   styleUrl: './university-view.component.scss'
 })
 export class UniversityViewComponent {
+
+  data: Project[] = mockData.cards;
+
 
 }
