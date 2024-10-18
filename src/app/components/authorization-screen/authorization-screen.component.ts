225fdaa7b9184f387eae1d43186fb058cab522a3
Przemek Gulik <gulikdev@gmail.com>
2024-10-18 19:55:27 +0200

    another part of registration form, autocomplete

--- src/app/components/authorization-screen/authorization-screen.component.ts
+++ src/app/components/authorization-screen/authorization-screen.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {Router, RouterOutlet} from "@angular/router";
 import {LocalStorageService} from "../../shared/service/local-storage.service";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
@@ -8,7 +8,10 @@ import {SnackbarService} from "../../shared/service/snackbar.service";
 import {SnackbarComponent, SnackbarData} from "../shared/snackbar/snackbar.component";
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatInput} from "@angular/material/input";
-
+import {NgScrollbar} from "ngx-scrollbar";
+import {startWith, take} from "rxjs";
+import {University} from "../../shared/constants/university";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 interface LoginForm {
   email: FormControl<string>,
@@ -19,13 +22,12 @@ interface RegisterForm extends LoginForm {
   passwordConfirmation: FormControl<string>,
   name: FormControl<string>,
   lastName: FormControl<string>,
-  university: any
+  university: FormControl<string>
 }
 
 type LoginData = {
   access_token: string;
 }
-
 
 @Component({
   selector: 'app-authorization-screen',
@@ -34,13 +36,16 @@ type LoginData = {
     RouterOutlet,
     ReactiveFormsModule,
     MatAutocompleteModule,
-    MatInput
+    MatInput,
+    NgScrollbar
   ],
   templateUrl: './authorization-screen.component.html',
   styleUrl: './authorization-screen.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AuthorizationScreenComponent implements OnInit {
+
+  destroyRef = inject(DestroyRef)
 
   loginFormFormGroup = new FormGroup<LoginForm>({
     email: new FormControl<string>('', {nonNullable: true, validators: [Validators.email, Validators.required]}),
@@ -51,27 +56,29 @@ export class AuthorizationScreenComponent implements OnInit {
   })
 
   registerFormFormGroup = new FormGroup<RegisterForm>({
-    email: new FormControl<string>('', {nonNullable: true, validators: [Validators.email, Validators.required]}),
-    password: new FormControl<string>('', {
-      nonNullable: true,
-      validators: [Validators.minLength(4), Validators.required]
-    }),
-    passwordConfirmation: new FormControl<string>('', {
-      nonNullable: true,
-      validators: [Validators.minLength(4), Validators.required]
-    }),
-    name: new FormControl<string>('', {nonNullable: true, validators: [Validators.minLength(3), Validators.required]}),
-    lastName: new FormControl<string>('', {
-      nonNullable: true,
-      validators: [Validators.minLength(2), Validators.required]
-    }),
-    university: new FormControl('')
-  })
+      email: new FormControl<string>('', {nonNullable: true, validators: [Validators.email, Validators.required]}),
+      password: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [Validators.minLength(4), Validators.required]
+      }),
+      passwordConfirmation: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [Validators.minLength(4), Validators.required]
+      }),
+      name: new FormControl<string>('', {nonNullable: true, validators: [Validators.minLength(3), Validators.required]}),
+      lastName: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [Validators.minLength(2), Validators.required]
+      }),
+      university: new FormControl<string>('', {nonNullable: true, validators: [Validators.required]})
+    }
+  )
 
   singUpMode: boolean = false;
   loading: boolean = false;
 
-  dummy = ['dpa', 'dupa']
+  universities: University[] = []
+  filteredUniversities: University[] = [];
 
   constructor(
     private readonly localStorage: LocalStorageService,
@@ -83,11 +90,13 @@ export class AuthorizationScreenComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getAllUniversities()
+    this.observeAutocompleteFilter()
   }
 
   submitRegistration() {
-    console.log(this.registerFormFormGroup)
-    if (!this.registerFormFormGroup.valid) {
+    if (!this.passwordsMatching()) {
+      // TODO Live form validation for password confirmations
       return;
     }
   }
@@ -106,20 +115,54 @@ export class AuthorizationScreenComponent implements OnInit {
           this.router.navigate([''])
         },
         error: (e: HttpErrorResponse) => {
-          if (e.status === 401) {
-
-            const data: SnackbarData = {
-              message: 'Niepoprawne dane logowania',
-              variant: "error",
-              closeButton: true
-            }
-
-            this.snackbarService.snackbarFromComponent(SnackbarComponent, data)
-          }
-          this.loading = false;
-          this.changeDetectorRef.detectChanges()
+          this.handleError(e);
         }
       })
   }
 
+
+  private getAllUniversities() {
+    this.httpService.get('universities').pipe(
+      take(1)
+    ).subscribe((universities: University[]) => {
+      this.universities = universities;
+      this.filteredUniversities = universities;
+      this.changeDetectorRef.detectChanges();
+    })
+  }
+
+  private observeAutocompleteFilter() {
+    this.registerFormFormGroup.controls.university.valueChanges.pipe(
+      startWith(''),
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe((value) => {
+      this.filteredUniversities = this.universities.filter(
+        university => university.name.toLowerCase().includes(value.toLowerCase())
+      )
+    })
+  }
+
+  private passwordsMatching() {
+    return this.registerFormFormGroup.controls.password.value === this.registerFormFormGroup.controls.passwordConfirmation.value
+  }
+
+  private handleError(e: HttpErrorResponse) {
+    let data: SnackbarData = {
+      message: 'Ups! Coś poszło nie tak :(',
+      variant: "error",
+      closeButton: true
+    }
+
+    if (e.status === 401) {
+      data = {
+        message: 'Niepoprawne dane logowania',
+        variant: "error",
+        closeButton: true
+      }
+    }
+
+    this.snackbarService.snackbarFromComponent(SnackbarComponent, data)
+    this.loading = false;
+    this.changeDetectorRef.detectChanges()
+  }
 }
--- src/app/shared/service/http.service.ts
+++ src/app/shared/service/http.service.ts
@@ -9,12 +9,17 @@ export class HttpService {
 
   constructor(
     private readonly httpClient: HttpClient
-  ) { }
-  
-  post(url: string, body: any):Observable<any> {
+  ) {
+  }
+
+  post(url: string, body: any): Observable<any> {
     return this.httpClient.post(`http://localhost:3000/${url}`, body)
   }
-  
+
+  get(url: string): Observable<any> {
+    return this.httpClient.get(`http://localhost:3000/${url}`)
+  }
+
 }
 
 
