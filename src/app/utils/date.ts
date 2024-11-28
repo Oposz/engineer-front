946e22a92eb5a022564c304176e7e5682d53d469
Przemek Gulik <gulikdev@gmail.com>
2024-11-28 20:18:16 +0100

    date displaying func bugfix

--- src/app/utils/date.ts
+++ src/app/utils/date.ts
@@ -1,13 +1,14 @@
 export function getDate(date: string) {
-  const endDate = new Date(date)
-  const day = endDate.getUTCDate()
-  const month = endDate.getUTCMonth() + 1
-  const year = endDate.getUTCFullYear()
-  return `${day}/${month}/${year}`
+  const endDate = new Date(date);
+  const localDate = endDate.toLocaleDateString('pl-PL');
+  return localDate.replace(/\./g, '/');
 }
 
 export function getRemainingDays(date: string) {
-  const endDate = new Date(date)
-  const now = new Date(Date.now())
-  return Math.floor((endDate.getTime() - now.getTime()) / (1000 * 60 * 60 * 24))
+  const endDate = new Date(date);
+  const now = new Date();
+  endDate.setHours(0, 0, 0, 0);
+  now.setHours(0, 0, 0, 0);
+  const differenceInTime = endDate.getTime() - now.getTime();
+  return Math.floor(differenceInTime / (1000 * 60 * 60 * 24));
 }
