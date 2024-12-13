5a205cac0a8cb30f0990f5965159f769c60af399
Przemek Gulik <gulikdev@gmail.com>
2024-12-13 01:35:43 +0100

    mobile view fixes, env mechanism

--- src/app/components/shared/mobile-menu/mobile-menu.component.ts
+++ src/app/components/shared/mobile-menu/mobile-menu.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {NavTabComponent} from "../../menu/nav-tab/nav-tab.component";
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {UserRole} from "../../../shared/constants/userRole";
+import {LocalStorageService} from "../../../shared/service/local-storage.service";
 
 @Component({
   selector: 'app-mobile-menu',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NavTabComponent,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './mobile-menu.component.html',
   styleUrl: './mobile-menu.component.scss'
 })
 export class MobileMenuComponent {
+  admin = this.localStorage.getItem('uniteam-user-role')
 
+  protected readonly UserRole = UserRole;
+
+  constructor(
+    private readonly localStorage: LocalStorageService
+  ) {
+  }
 }
--- src/app/shared/service/http.service.ts
+++ src/app/shared/service/http.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -13,19 +14,19 @@ export class HttpService {
   }
 
   post(url: string, body: any): Observable<any> {
-    return this.httpClient.post(`http://localhost:3000/${url}`, body)
+    return this.httpClient.post(`${environment.apiUrl}${url}`, body)
   }
 
   get(url: string): Observable<any> {
-    return this.httpClient.get(`http://localhost:3000/${url}`)
+    return this.httpClient.get(`${environment.apiUrl}${url}`)
   }
 
   patch(url: string, body: any): Observable<any> {
-    return this.httpClient.patch(`http://localhost:3000/${url}`, body)
+    return this.httpClient.patch(`${environment.apiUrl}${url}`, body)
   }
 
   delete(url:string){
-    return this.httpClient.delete(`http://localhost:3000/${url}`)
+    return this.httpClient.delete(`${environment.apiUrl}${url}`)
   }
 
 }
